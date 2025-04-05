a9d05163f8767fe77db37a4c9505b9eaa1a8efc4
Muhammad Umer Khan <muhammadumerk546@gmail.com>
2025-04-05 20:19:19 +0500

    Update config.py

--- config/config.py
+++ config/config.py
@@ -6,12 +6,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Logging configuration
-os.makedirs("logs", exist_ok=True)
+os.makedirs(os.path.join("..", "logs"), exist_ok=True)
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
-        logging.FileHandler("logs/document_classifier.log"),
+        logging.FileHandler(os.path.join("..", "logs", "config.log")),
         logging.StreamHandler()
     ]
 )
@@ -23,11 +23,18 @@ except:
     logging.error("❌ API KEYS not found or not set.")
 
 try:
+    CLAUSE_EXTRACTION_PROMPT_PATH = os.path.join("prompts", "clause_extraction.txt")
+    RISK_ANALYZER_PATH = os.path.join("prompts", "risk_analysis.txt")
+    DOC_CLASSIFICATION_PATH = os.path.join("prompts", "document_classification.txt")
+    DOC_SUMMARIZER_PATH = os.path.join("prompts", "summarization.txt")
+except FileNotFoundError as f:
+    logging.error(f"❌ {f.filename} not found.")
+try:
     # Constants
-    CHUNK_SIZE = 1000
-    CHUNK_OVERLAP = 200
+    CHUNK_SIZE = int(1000)
+    CHUNK_OVERLAP = int(200)
     MODEL_NAME = "qwen-2.5-32b"
-    MAX_TEXT_LIMIT = 3000
+    MAX_TEXT_LIMIT = int(3000)
 except Exception as e:
     logging.error(f"❌ Error loading Constants: {str(e)}")
 
