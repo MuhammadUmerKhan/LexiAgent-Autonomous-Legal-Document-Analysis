850abf80724b21b4a131099593f4601fd1f99781
Muhammad Umer Khan <muhammadumerk546@gmail.com>
2025-04-05 15:22:48 +0500

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,8 @@
 from langchain_groq import ChatGroq
 from config import config as CONFIG
+import streamlit as st
+from sentence_transformers import SentenceTransformer
+from langchain.embeddings import HuggingFaceEmbeddings
 import os, logging
 
 os.makedirs("logs", exist_ok=True)
@@ -40,3 +43,23 @@ def configure_llm():
     except Exception as e:
         logging.error(f"❌ LLM Query Error: {str(e)}")
         return "❌ Error generating LLM response."
+
+@st.cache_resource  # Cache the embedding model to avoid reloading it every time
+def configure_embedding_model():
+    """
+    Configures and caches the embedding model.
+
+    Returns:
+        embedding_model (FastEmbedEmbeddings): The loaded embedding model.
+    """
+    return SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")  # Load and return the embedding model
+
+@st.cache_resource
+def configure_vector_embeddings():
+    """
+    Configures and caches the vector embeddings for Groq API.
+
+    Returns:
+        vector_embeddings (HuggingFaceEmbeddings): The loaded vector embeddings.
+    """
+    return HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")  # Load and return the vector embeddings
