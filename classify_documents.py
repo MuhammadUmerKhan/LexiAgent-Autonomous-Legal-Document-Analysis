70b1bd5e58dbca804a6845561fa0ea6f1ba70cf7
Muhammad Umer Khan <muhammadumerk546@gmail.com>
2025-04-05 20:19:10 +0500

    Update classify_documents.py

--- classify_documents.py
+++ classify_documents.py
@@ -1,10 +1,8 @@
-import os
-import logging
+import os, logging
 from typing import Optional
 from utils.utils import configure_llm, load_prompt_template
 from document_loader import load_document
 from config import config as CONFIG
-from pathlib import Path
 
 # Setup logging
 os.makedirs("logs", exist_ok=True)
@@ -17,10 +15,10 @@ logging.basicConfig(
     ]
 )
 
-def classify_document(text: str, prompt_path: str) -> Optional[str]:
+def classify_document(text: str) -> Optional[str]:
     try:
         # Load prompt template
-        prompt_template = load_prompt_template(prompt_path)
+        prompt_template = load_prompt_template(CONFIG.DOC_CLASSIFICATION_PATH)
         prompt = prompt_template.replace("{text}", text.strip()[:CONFIG.MAX_TEXT_LIMIT])  # Limit size for efficiency
 
         logging.info("🔍 Sending document to LLM for classification...")
@@ -39,14 +37,14 @@ def classify_document(text: str, prompt_path: str) -> Optional[str]:
         logging.error(f"❌ Error during classification: {e}")
         return None
 
-def get_classified_doc(file: str, prompt_path: str):
+def get_classified_doc(file: str):
     text = load_document(file)
-    return classify_document(text, prompt_path)
+    return classify_document(text)
 
 if __name__ == "__main__":
     # Sample test
     file_path = "./data/attention paper.pdf"
-    prompt_path = "./prompts/document_classification.txt"
-    result = get_classified_doc(file_path, prompt_path=prompt_path)
+    result = get_classified_doc(file_path)
+    
     if result:
         print(f"📄 Document Type: {result}")
